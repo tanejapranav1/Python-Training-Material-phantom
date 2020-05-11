48834f653ee46b4100ee9e8ccfbef42e1767ab2a
prataneja <63849342+prataneja@users.noreply.github.com>
2020-05-11 12:47:38 +0530

    Initial Commit

---
+++ test.py
@@ -0,0 +1,18 @@
+"""A test module.
+
+It contains a class and a function.
+"""
+
+
+class Thing:
+    """This is a test class."""
+
+    def thingy(self):
+        """This is a test method."""
+        print("hello")
+
+
+def do_hello():
+    """This is a test function."""
+    thing = Thing()
+    thing.thingy()
